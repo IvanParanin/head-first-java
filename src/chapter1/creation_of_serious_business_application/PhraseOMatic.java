4396d98be2cd047e7e69a0a02b445547725a2e9a
Ivan <ivanparanin@bk.ru>
2023-02-17 15:15:45 +0600

    add PhraseOMatic class

---
+++ src/chapter1/creation_of_serious_business_application/PhraseOMatic.java
@@ -0,0 +1,35 @@
+package chapter1.creation_of_serious_business_application;
+
+
+public class PhraseOMatic {
+    public static void main (String[] args){
+
+        //Создайте три набора слов для выбора. Добавляйте собственные слова!
+
+        String[] wordListOne = {"круглосуточный", "трех-звенный", "30000-футовый", "взаимный", "обоюдный выигрыш", "фронтенд", "на основе веб-технологий", "проникающий", "умный", "шесть сигм", "метод критического пути", "динамичный"};
+
+        String[] wordListTwo = {"уполномоченный", "трудный", "чистый продукт", "ориентированыый", "центральный", "распределенный", "кластеризованный", "фирмнный", "нестандартный ум", "позиционированный", "сетевой", "сфокусированный", "использованный с выгодой", "выровненный", "нацеленный на", "общий", "совместный", "ускоренный"};
+
+        String[] wordListThree = {"процесс", "пункт разгрузки", "выход из положения", "тип структуры", "талант", "подход", "уровень завоеванного внимания", "портал", "период времеи", "обзор", "образец", "пункт следования"};
+
+        //Вычисляем, сколько слов в каждом списке
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        //Генерируем три случайных числа
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        //Теперь строим фразу
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        //Выводим фразу на экран
+
+        System.out.println("Все, что нам нужно,-это " + phrase);
+    }
+}
